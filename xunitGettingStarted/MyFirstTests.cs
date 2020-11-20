e315ed5134cdf38141b69e391978a6a4cd07611a
Terry Mace <terry.mace@bglgroup.co.il>
2020-11-20 16:08:05 +0000

    Created some tests

---
+++ xunitGettingStarted/MyFirstTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xunitGettingStarted
+{
+    public class MyFirstTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
